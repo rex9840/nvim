e82b314bb8695bcf01eacf0943e25f61b43b07c6
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-12-06 23:53:16 +0545

    added plugins barbar

--- lua/plugins/qos_programing.lua
+++ lua/plugins/qos_programing.lua
@@ -68,4 +68,21 @@ return
 
 
 ----------------------------------------------------------------------
+
+-- barbar plugins
+
+{
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = true end,
+    opts = {},
+    version = '^1.0.0',
+},
+
+
+------------------------------------------------------------------------
+
 }
