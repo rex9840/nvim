5a6588617d544e6dff90973bec7502877fe20670
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-12-07 07:57:58 +0545

    config and theme chnaged

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -7,6 +7,7 @@ print("--keymaps initiated--")
 
 
 require("keymaps.error_diagnosis")
+require("keymaps.barbar")
 
 
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,6 +39,31 @@ require('telescope').setup()
 ---------------------------------------------------------------------
 
 -- identation color setup
+
 require("ibl").setup()
+
 ----------------------------------------------------------------------
+
+-- barbar setup -- 
+
+require'barbar'.setup
+{
+	
+	sidebar_filetypes = {
+	    -- Use the default values: {event = 'BufWinLeave', text = nil}
+	    NvimTree = true,
+	    -- Or, specify the text used for the offset:
+	    undotree = {text = 'undotree'},
+	    -- Or, specify the event which the sidebar executes when leaving:
+	    ['neo-tree'] = {event = 'BufWipeout'},
+	    -- Or, specify both
+	    Outline = {event = 'BufWinLeave', text = 'symbols-outline'},
+	  },
+
+
+
+}
+
+
+
 
