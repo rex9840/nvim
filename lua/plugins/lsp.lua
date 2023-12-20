3da4619c4aab7608e47ebd312e2f8d3b599f33a8
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-12-20 19:58:38 +0545

    removed and restored lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -42,22 +42,6 @@ return{
 
  ---------------------------------------------------------------------
 
-    -- Lsp saga for more lsp configs
-
-{
-    'nvimdev/lspsaga.nvim',
-    config = function()
-        require('lspsaga').setup({})
-    end,
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter', -- optional
-        'nvim-tree/nvim-web-devicons',     -- optional
-	'neovim/nvim-lspconfig',
-    },
-    event = 'LspAttach'
-},
-
-
 
 ---------------------------------------------------------------------
 }
