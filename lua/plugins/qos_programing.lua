f9175e3cd840f7c3d586c7c5184aa11b01d582fd
Rupesh Nepal <rupeshrnepal@gmail.com>
2023-12-31 13:18:08 +0545

    git plugins removed

--- lua/plugins/qos_programing.lua
+++ lua/plugins/qos_programing.lua
@@ -2,6 +2,23 @@
 return
 {
 ---------------------------------------------------------
+
+-- auto complition -- 
+{
+'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+},
 ------------------------------------------------------------------
 
 	--revover.vim for swap file handelling
