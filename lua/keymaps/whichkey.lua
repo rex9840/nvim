f8e6ec8216c5f66fb95e7546eef4ed677859264f
Rupesh Nepal <rupeshrnepal@gmail.com>
2024-01-27 20:30:02 +0545

    remove : which_key shortcut creating a conflict logiacally

--- lua/keymaps/whichkey.lua
+++ lua/keymaps/whichkey.lua
@@ -1,8 +1,6 @@
 
 
 require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>f'] = {name = '[F]ind',_ = 'which_key_ignore'},
   ['<leader>b'] = {name = '[B]uffer',_ = 'which_key_ignore'},
 }
