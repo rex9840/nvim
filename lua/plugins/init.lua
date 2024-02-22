dd205f676866d283347a44ba37b8be1ec3436854
Rupesh Nepal <rupeshrnepal@gmail.com>
2024-02-22 17:10:09 +0545

    Fixed virtual text for lsp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,6 +37,17 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+  
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- disable virtual text
+        virtual_text = false,
+        -- show signs
+        signs = true,
+      }
+  )
+
+
 end
 
 
