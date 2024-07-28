f9cf75d07aa7a2252cf8e5bfff1ce00c8d185ab2
Rupesh Nepal <rupeshrnepal@gmail.com>
2024-07-28 08:49:50 +0545

    feat : new keymap config for updated keymap plugin

--- lua/keymaps/whichkey.lua
+++ lua/keymaps/whichkey.lua
@@ -1,5 +1,20 @@
-require('which-key').register {
-        ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
-        ['<leader>b'] = { name = '[B]uffer or [B]rowse ', _ = 'which_key_ignore' },
-        ['<leader>m'] = { name = 'for[M]at', _ = 'which_key_ignore' },
-}
+-- older key map in which key 
+
+-- require('which-key').register {
+--         ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
+--         ['<leader>b'] = { name = '[B]uffer or [B]rowse ', _ = 'which_key_ignore' },
+--         ['<leader>m'] = { name = 'for[M]at', _ = 'which_key_ignore' },
+-- }
+
+
+require('which-key').add(
+        {
+                { '<leader>f',  group = '[F]ile' },
+                { '<leader>f_', hidden = true,     group = '[F]ile' },
+                { '<leader>b',  group = '[B]uffer' },
+                { '<leader>b_', hidden = true,     group = '[B]uffer' },
+                { '<leader>m',  group = 'for[M]at' },
+                { '<leader>m_', hidden = true,     group = 'for[M]at' },
+
+        }
+)
--- lua/plugins/qos_programing.lua
+++ lua/plugins/qos_programing.lua
@@ -43,13 +43,16 @@ return
         -- key binding helper
         {
                 'folke/which-key.nvim',
+                dependencies = {
+                        "echasnovski/mini.icons",
+                        "nvim-tree/nvim-web-devicons",
+                },
                 event = "VeryLazy",
                 init = function()
                         vim.o.timeout = true
                         vim.o.timeoutlen = 300
                 end,
-                opts = {}
-
+                opts = {},
         },
 
         ---------------------------------------------------------------------
