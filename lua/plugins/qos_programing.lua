331809c5d4cbf98eece6c9cd1a9e76d64495eaff
Rupesh Nepal <rupeshrnepal@gmail.com>
2025-03-10 12:00:39 +0545

    feat(telescope-file-browser): added extensions of telescope

--- lua/plugins/qos_programing.lua
+++ lua/plugins/qos_programing.lua
@@ -1,31 +1,30 @@
-return
-{
+return {
         ---------------------------------------------------------
 
         -- auto complition --
         {
-                'hrsh7th/nvim-cmp',
+                "hrsh7th/nvim-cmp",
                 dependencies = {
                         -- Snippet Engine & its associated nvim-cmp source
-                        'L3MON4D3/LuaSnip',
-                        'saadparwaiz1/cmp_luasnip',
+                        "L3MON4D3/LuaSnip",
+                        "saadparwaiz1/cmp_luasnip",
 
                         -- Adds LSP completion capabilities
-                        'hrsh7th/cmp-nvim-lsp',
-                        'hrsh7th/cmp-path',
+                        "hrsh7th/cmp-nvim-lsp",
+                        "hrsh7th/cmp-path",
 
                         -- Adds a number of user-friendly snippets
-                        'rafamadriz/friendly-snippets',
+                        "rafamadriz/friendly-snippets",
                 },
         },
         ------------------------------------------------------------------
 
         -- Fuzzy Finder (files, lsp, etc)
         {
-                'nvim-telescope/telescope.nvim',
+                "nvim-telescope/telescope.nvim",
                 dependencies = {
-                        'nvim-lua/plenary.nvim',
-                        'nvim-telescope/telescope-fzf-native.nvim',
+                        "nvim-lua/plenary.nvim",
+                        "nvim-telescope/telescope-fzf-native.nvim",
                 },
         },
         ----------------------------------------------------------------------
@@ -33,16 +32,16 @@ return
         {
 
                 -- Highlight, edit, and navigate code
-                'nvim-treesitter/nvim-treesitter',
+                "nvim-treesitter/nvim-treesitter",
                 dependencies = {
-                        'nvim-treesitter/nvim-treesitter-textobjects',
+                        "nvim-treesitter/nvim-treesitter-textobjects",
                 },
-                build = ':TSUpdate',
+                build = ":TSUpdate",
         },
 
         -- key binding helper
         {
-                'folke/which-key.nvim',
+                "folke/which-key.nvim",
                 dependencies = {
                         "echasnovski/mini.icons",
                         "nvim-tree/nvim-web-devicons",
@@ -57,27 +56,31 @@ return
 
         ---------------------------------------------------------------------
 
-
         -- auto paring of brases columns
 
         {
-                'windwp/nvim-autopairs',
+                "windwp/nvim-autopairs",
                 event = "InsertEnter",
                 config = true,
-                opts = {}
+                opts = {},
         },
 
+        ---------------------------------------------------------------------
+        --- Telescope file browser
+
+        {
+                "nvim-telescope/telescope-file-browser.nvim",
+                dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+        },
 
         ---------------------------------------------------------------------
 
-
-
         -- comment.vim
         {
-                'numToStr/Comment.nvim',
+                "numToStr/Comment.nvim",
                 opts = {
                         -- add any options here
                 },
                 lazy = false,
-        }
+        },
 }
