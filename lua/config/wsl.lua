a8568ff588687574a501f0bb104049e3397a1b98
rex9840 <rupeshrnepal@gmail.com>
2025-05-18 15:28:06 +0545

    refactor(wsl): wsl clipboard yank32 made it into function

--- lua/config/wsl.lua
+++ lua/config/wsl.lua
@@ -1,5 +1,7 @@
 local win32yank = vim.fn.executable("win32yank.exe")
-if win32yank == 1 then 
+
+local function set_up_yank32_clipboard()
+
         vim.g.clipboard = {
                 name = "win32yank",
                 copy = {
@@ -14,6 +16,10 @@ if win32yank == 1 then
         }
 end
 
+if win32yank == 1 then 
+        set_up_yank32_clipboard()
+end
+
 -- copilot  node configuration
 
 local node_version = "v20.19.1"
@@ -25,17 +31,7 @@ if distribution == "Ubuntu" then
         vim.g.copilot_node_command = string.format("~/.nvm/versions/node/%s/bin/node", node_version)
 
         -- install win32 yank for clipboard sync and support in wsl
-        vim.g.clipboard = {
-                name = "win32yank",
-                copy = {
-                        ["+"] = "win32yank.exe -i --crlf",
-                        ["*"] = "win32yank.exe -i --crlf",
-                },
-                paste = {
-                        ["+"] = "win32yank.exe -o --lf",
-                        ["*"] = "win32yank.exe -o --lf",
-                },
-                cache_enabled = 0,
-        }
+        
+        set_up_yank32_clipboard()
 end
 
