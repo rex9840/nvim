9022b941a3a2a9a7ed57866068b47a48943b4976
Rupesh Nepal <rupeshrnepal@gmail.com>
2025-07-13 08:58:33 +0545

    feat : tree-sitter  highlight config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -246,7 +246,11 @@ require("nvim-treesitter.configs").setup({
                 "yaml",
         },
         auto_install = true,
-        highlight = { enable = true },
+        highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+
+        },
         indent = { enable = true },
 })
 
